ccdf61f869e61e578e9f9d942afde7acbde2cfc0
Android Studio <devandroid@ort>
2025-08-22 12:13:30 -0300

    solucionamos login

--- Backoffice/src/components/Login.jsx
+++ Backoffice/src/components/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
 import './Login.css';
 
 
-export default function Login() {
+/*export default function Login() {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -28,13 +29,93 @@ export default function Login() {
         throw new Error('Error: usuario y/o contraseña incorrecto');
       }
 
-      localStorage.setItem('token', data.token);*/
+      localStorage.setItem('token', data.token);
       navigate('/dashboard/home');
 
     } catch (err) {
       setError(err.message || 'Error: usuario y/o contraseña incorrecto');
     }
-  };
+  };*/
+  const LogIn = () => {
+    const [form, setForm] = useState({
+      username: "",
+      password: "",
+    });
+    const [errors, setErrors] = useState({});
+    const [success, setSuccess] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+      setForm({
+        ...form,
+        [e.target.name]: e.target.value,
+      });
+      setErrors({
+        ...errors,
+        [e.target.name]: "",
+      });
+    };
+
+    const validate = () => {
+      let newErrors = {};
+      if (!form.username) newErrors.username = "Email required";
+      if (!form.password) newErrors.password = "Password required";
+      return newErrors;
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const validationErrors = validate();
+      if (Object.keys(validationErrors).length > 0) {
+        setErrors(validationErrors);
+        setSuccess(false);
+        return;
+      }
+
+      setErrors({});
+      setSuccess(false);
+
+    
+
+    try {
+      const url = new URL (`http://localhost:3000/usuarios/loginBack`);
+      console.log(success);
+      const response = await axios.post(
+        url.toString(),
+        {
+          username: form.username,
+          password: form.password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": 1,
+          },
+        }
+      );
+
+      console.log("Login response:", response.data);
+      sessionStorage.setItem("token", response.data.token);
+      sessionStorage.setItem("user", response.data.ID);
+
+      console.log(sessionStorage)
+
+      setSuccess(true);
+      setTimeout(() => {
+        navigate("/home");
+      }, 200);
+
+    } catch (err) {
+      setErrors({
+        ...errors,
+        password:
+          err.response?.data?.message ||
+          "Login failed (400). Revisa usuario y contraseña.",
+      });
+      setSuccess(false);
+    }
+  }
+
 
   return (
     <div className="contenedor-autenticacion">
@@ -42,31 +123,45 @@ export default function Login() {
       <div className="caja-autenticacion">
         <h2>¡Bienvenido/a de nuevo!</h2>
 
-        <label>Nombre de Usuario</label>
-        <input 
-          type="text" 
-          placeholder="Nombre de Usuario" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <form onSubmit={handleSubmit}>
 
-        <label>Contraseña</label>
-        <input 
-          type="password" 
-          placeholder="Contraseña" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <label>Nombre de Usuario</label>
+          <input 
+            type="text"
+            name ="username" 
+            value={form.username} 
+            placeholder="Nombre de Usuario" 
+            onChange={handleChange}
+          />
+           {errors.username && (
+                <span>{errors.username}</span>
+              )}
 
-        {error && <p className="error">{error}</p>}
 
-        <button 
-          className="boton-autenticacion" onClick={handleLogin}>Ingresar</button>
 
-        <p className="enlace-autenticacion">
-          ¿No tienes usuario? <Link to="/register">Registrate</Link>
-        </p>
+          <label>Contraseña</label>
+          <input 
+            type="password" 
+            name="password"
+            placeholder="Contraseña" 
+            value={form.password} 
+            onChange={handleChange}
+            autoComplete="current-password"
+          />
+
+              {errors.password && (
+                <span>{errors.password}</span>
+              )}
+
+          <button className="boton-autenticacion">Ingresar</button>
+
+          <p className="enlace-autenticacion">
+            ¿No tienes usuario? <Link to="/register">Registrate</Link>
+          </p>
+        </form>
       </div>
     </div>
   );
-}
+};
+
+export default LogIn;
